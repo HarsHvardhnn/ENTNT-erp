472cd6074518e930954a155e961ab5acbca44e9b
Harsh Vardhan Chawla <148744120+HarsHvardhnn@users.noreply.github.com>
2024-03-15 10:04:11 +0530

    Update NavBar.jsx to make it responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className="flex justify-between p-4 font-bold text-gray-500">
@@ -13,22 +19,53 @@ const NavBar = () => {
             <p>@Harsh (click me) </p>
           </a>
         </div>
-        <div className="flex gap-12">
+        <div className="md:hidden">
+          <button
+            onClick={toggleMenu}
+            className="focus:outline-none text-gray-500 hover:text-gray-900"
+          >
+            <svg
+              className="h-6 w-6 fill-current"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4 6h16a1 1 0 010 2H4a1 1 0 110-2zm0 5h16a1 1 0 010 2H4a1 1 0 010-2zm0 5h16a1 1 0 010 2H4a1 1 0 010-2z"
+              />
+            </svg>
+          </button>
+        </div>
+        <div className="hidden md:flex gap-12">
           <NavLink to="/" className="inline-block">
-            <p className="group-hover:before:absolute  group-hover:before:block group-hover:before:content-[''] group-hover:before:bg-black group-hover:before:h-[2px] group-hover:before:w-0 group-hover:before:transition-all group-hover:before:left-0 group-hover:before:bottom-0"></p>
-            <p className="hover:text-blue-400 ">DashBoard</p>
+            <p className="hover:text-blue-400">Dashboard</p>
           </NavLink>
           <NavLink to="/product">
-            <p className=" hover:text-blue-400 active:bg-red">Products</p>
+            <p className="hover:text-blue-400">Products</p>
           </NavLink>
           <NavLink to="/order">
-            <p className="hover:text-blue-400 ">Orders</p>
+            <p className="hover:text-blue-400">Orders</p>
           </NavLink>
         </div>
         <div>
           <NavLink>@credits:Harsh</NavLink>
         </div>
       </div>
+      {isOpen && (
+        <div className="md:hidden">
+          <div className="flex flex-col gap-4 bg-gray-100 p-4">
+            <NavLink to="/" className="inline-block">
+              <p className="hover:text-blue-400">Dashboard</p>
+            </NavLink>
+            <NavLink to="/product">
+              <p className="hover:text-blue-400">Products</p>
+            </NavLink>
+            <NavLink to="/order">
+              <p className="hover:text-blue-400">Orders</p>
+            </NavLink>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
